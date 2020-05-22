25f0bb1395a7728672103ae524ad6975d2043d4e
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-05-22 09:04:40 -0700

    Add pets result

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cickatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
-
-  /*
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-  ]);
-  */
 };
 
-//render(React.createElement(App), document.getElementById("root"));
 render(<App />, document.getElementById("root"));
