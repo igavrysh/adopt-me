b297d328f8f565258aebf275e14fefbba6991d45
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-05-25 08:48:57 -0700

    Add spinning pudle

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
 import colors from "./Colors";
 
-const color = "pink";
+const spin = keyframes`
+  to {
+    transform: rotate(360deg)
+  }
+`;
 
 const NavBar = () => {
   const [padding, setPadding] = useState(15);
@@ -19,7 +23,10 @@ const NavBar = () => {
       <span
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
           &:hover {
+            animation: 1s ${spin} linear infinite reverse;
             text-decoration: underline;
           }
         `}
